457b08b61a5bbc84a6deb0f6aba60c83b1d902ea
victorkok <victornerikokado@yahoo.com>
2021-08-31 22:26:26 -0300

    Transição de todo POSTGRESQL do Produto

--- project/form.py
+++ project/form.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import Consumer, Product, Destiny
+from flask import Flask, jsonify, request
+
+from .entities.entity import Session, engine, Base
+from .entities.produto import Produto
+from .entities.produto import Produto, ProdutoSchema
 
 form = Blueprint('form', __name__)
 
@@ -45,65 +50,105 @@ def edit_consumer(id):
         return redirect(url_for('form.consumer'))
     return render_template('consumer/edit_consumer.html', consumer=consumer)
 
-@form.route('/delete_consumer/<int:id>')
-def delete_consumer(id):
-    consumer = Consumer.query.get(id)
-    db.session.delete(consumer)
-    db.session.commit()
-    return redirect(url_for('form.consumer'))
-
-
-@form.route('/product')
-def product():
-    products = Product.query.all()
-    print(product) 
-    return render_template('product/product.html', products=products)
-
-
-@form.route("/add_product", methods=["GET", "POST"])
-def add_product():
-    if request.method == 'POST':
-        product = Product(name=request.form['name'],
-                          price=request.form['price'].replace(",", "."),
-                          description=request.form['description'],
-                          players=request.form['players'],
-                          age=request.form['age'])
-        db.session.add(product)
-        db.session.commit()
-        return redirect(url_for('form.product'))
-    return render_template('product/add_product.html')
-
+# Abaixo rotas e codigo Flask de POSTGRESQL
 
 @form.route('/edit_product/<int:id>', methods=['GET', 'POST'])
 def edit_product(id):
-    product = Product.query.get(id)
+    session = Session()
+    product = session.query(Produto).get(id)
     if request.method == 'POST':
-        product.name = request.form['name']
-        product.price = request.form['price']
+        product.nome = request.form['nome']
         product.description = request.form['description']
-        product.players = request.form['players']
-        product.age = request.form['age']
-        db.session.commit()
-        print('comitou')
-        return redirect(url_for('form.product'))
+        product.preco = request.form['preco']
+        product.editora = request.form['editora']
+        product.faixa_etaria = request.form['faixa_etaria']
+        product.numero_de_jogadores = request.form['numero_de_jogadores']
+        session.commit()
+        return redirect(url_for('form.get_produtos'))
     return render_template('product/edit_product.html', product=product)
+
+Base.metadata.create_all(engine)
+
+
+@form.route('/product/<int:id>')
+def get_produto_id(id):
+    session = Session()
+    product = session.query(Produto).get(id)
+    return render_template('produto_id.html', product=product)
 
 
 @form.route('/delete_product/<int:id>')
 def delete_product(id):
-    product = Product.query.get(id)
-    db.session.delete(product)
-    db.session.commit()
-    return redirect(url_for('form.product'))
+    session = Session()
+    product = session.query(Produto).get(id)
+    session.delete(product)
+    session.commit()
+    return redirect(url_for('form.get_produtos'))
 
+@form.route('/')
+def home_get_produtos():
+    # fetching from the database
+    session = Session()
+    produto_objects = session.query(Produto).all()
 
-@form.route('/' , methods=["GET"])
-def home_get_product():
-    products = Product.query.all()
-    print(product) 
-    return render_template('index.html', products=products)
+    # transforming into JSON-serializable objects
+    schema = ProdutoSchema(many=True)
+    produtos = schema.dump(produto_objects)
 
-@form.route('/product/<int:id>')
-def get_produto_id(id):
-    product = Product.query.get(id)
-    return render_template('produto_id.html', product=product)
+    # serializing as JSON
+    session.close()
+    return render_template('index.html', produtos=produtos)
+
+@form.route('/produtos')
+def get_produtos():
+    # fetching from the database
+    session = Session()
+    produto_objects = session.query(Produto).all()
+
+    # transforming into JSON-serializable objects
+    schema = ProdutoSchema(many=True)
+    produtos = schema.dump(produto_objects)
+
+    # serializing as JSON
+    session.close()
+    return render_template('product/product.html', produtos=produtos)
+
+@form.route('/add_product', methods=['GET'])
+def get_add_product():
+    return render_template('product/add_product.html')
+
+@form.route('/add_product', methods=['POST'])
+def add_product():
+    # mount produto object
+    produto = Produto(nome=request.form['nome'],
+                          description=request.form['description'],
+                          editora=request.form['editora'],
+                          preco=request.form['preco'].replace(",", "."),
+                          faixa_etaria=request.form['faixa_etaria'],
+                          numero_de_jogadores=request.form['numero_de_jogadores'])
+
+    # persist produto
+    session = Session()
+    session.add(produto)
+    session.commit()
+
+    # return template
+    session.close()
+    return render_template('product/add_product.html'), 201
+
+"""
+@form.route('/produtos1')
+def get_produtos1():
+    # fetching from the database
+    session = Session()
+    produto_objects = session.query(Produto).all()
+
+    # transforming into JSON-serializable objects
+    schema = ProdutoSchema(many=True)
+    produtos = schema.dump(produto_objects)
+
+    # serializing as JSON
+    session.close()
+    return jsonify(produtos),201
+
+"""
